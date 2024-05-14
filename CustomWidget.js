7187319d59f772b37ff779827fe001f4acd85b86
Andrea Terrone <122184178+TerroneAndrea@users.noreply.github.com>
2024-05-14 16:20:39 +0200

    Update CustomWidget.js

--- CustomWidget.js
+++ CustomWidget.js
@@ -23,19 +23,22 @@
       this.canvas = document.createElement("canvas");
       this._shadowRoot.getElementById("root").appendChild(this.canvas);
 
-      // Set up some sample data
-      //this.data = [10, 20, 30, 40, 100];
+      // Create number formatter
+      this.formatter = new Intl.NumberFormat('en-US', {
+        minimumFractionDigits: 1,
+        maximumFractionDigits: 1,
+      });
 
       this.render();
     }
 
     connectedCallback() {
       this.render();
-  }
+    }
 
-  onCustomWidgetAfterUpdate (oChangedProperties) {
-    this.render();
-  }
+    onCustomWidgetAfterUpdate(oChangedProperties) {
+      this.render();
+    }
 
     onCustomWidgetResize(width, height) {
       this.render();
@@ -44,22 +47,19 @@
     async render() {
       // Get the canvas context
       const dataBinding = this.myDataBinding;
-      if (!dataBinding || dataBinding.state !== 'success') { 
-
-
       const ctx = this.canvas.getContext("2d");
-      ctx.drawImage('./reply.png', 0, 0);
 
-       }
-      else {
-        
+      if (!dataBinding || dataBinding.state !== 'success') {
+        const img = new Image();
+        img.src = './reply.png';
+        img.onload = () => {
+          ctx.drawImage(img, 0, 0);
+        };
+      } else {
         this.data = [];
         var rawMeasures = dataBinding.data[0]; // Select the first item in the 'data' array
         this.data = Object.keys(rawMeasures).map(key => rawMeasures[key].raw);
-          
       }
-
-      const ctx = this.canvas.getContext("2d");
 
       // Clear the canvas
       ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -73,12 +73,7 @@
       const height = this.canvas.height - margin.top - margin.bottom;
 
       const barWidth = width / this.data.length;
-      
       const columnLabels = ['INITIAL', 'AWARDED', 'FORECAST', 'ROLLING', 'GAP'];
-
-
-
-      
 
       // Draw bars using a for loop
       for (let i = 0; i < this.data.length; i++) {
@@ -88,18 +83,7 @@
         else if (i === this.data.length - 1) barColor = "red"; // Last number in red
         else barColor = "grey"; // Other numbers in grey
 
-        if (i===0 || i===1) {
-        ctx.fillStyle = barColor;
-        ctx.fillRect(
-          margin.left + i * barWidth + 10, // Add some separation between bars
-          margin.top + (1 - d / Math.max(...this.data)) * height,
-          barWidth - 20, // Reduce the width to include separation
-          d / Math.max(...this.data) * height
-        );
-        }
-        else if (i===2) {
-          
-          d = this.data[i] + this.data[i-1] ;
+        if (i === 0 || i === 1) {
           ctx.fillStyle = barColor;
           ctx.fillRect(
             margin.left + i * barWidth + 10, // Add some separation between bars
@@ -107,73 +91,77 @@
             barWidth - 20, // Reduce the width to include separation
             d / Math.max(...this.data) * height
           );
-          
-          ctx.fillStyle = "rgba(255, 255, 255, 255)";
+        } else if (i === 2) {
+          d = this.data[i] + this.data[i - 1];
+          ctx.fillStyle = barColor;
           ctx.fillRect(
             margin.left + i * barWidth + 10, // Add some separation between bars
-            margin.top + (1 - this.data[i-1] / Math.max(...this.data)) * height,
+            margin.top + (1 - d / Math.max(...this.data)) * height,
             barWidth - 20, // Reduce the width to include separation
-            this.data[i-1] / Math.max(...this.data) * height
+            d / Math.max(...this.data) * height
           );
 
+          ctx.fillStyle = "rgba(255, 255, 255, 1)";
+          ctx.fillRect(
+            margin.left + i * barWidth + 10, // Add some separation between bars
+            margin.top + (1 - this.data[i - 1] / Math.max(...this.data)) * height,
+            barWidth - 20, // Reduce the width to include separation
+            this.data[i - 1] / Math.max(...this.data) * height
+          );
+        } else {
+          ctx.fillStyle = barColor;
+          ctx.fillRect(
+            margin.left + i * barWidth + 10, // Add some separation between bars
+            margin.top + (1 - d / Math.max(...this.data)) * height,
+            barWidth - 20, // Reduce the width to include separation
+            d / Math.max(...this.data) * height
+          );
         }
-        else {
 
-        ctx.fillStyle = barColor;
-        ctx.fillRect(
-          margin.left + i * barWidth + 10, // Add some separation between bars
-          margin.top + (1 - d / Math.max(...this.data)) * height,
-          barWidth - 20, // Reduce the width to include separation
-          d / Math.max(...this.data) * height
+        // Format the number
+        const formattedValue = this.formatter.format(d);
+
+        // Draw the number above the bar
+        if (i === 2) {
+          d = d - this.data[i - 1];
+          const formattedInnerValue = this.formatter.format(d);
+          ctx.fillStyle = "black";
+          ctx.font = "14px Arial";
+          ctx.textAlign = "center";
+          ctx.fillText(
+            formattedInnerValue, // Use formatted value
+            margin.left + i * barWidth + barWidth / 2, // Center text horizontally
+            margin.top + (1 - (d + this.data[i - 1]) / Math.max(...this.data)) * height - 5 // Place text above the bar
+          );
+        } else {
+          ctx.fillStyle = "black";
+          ctx.font = "14px Arial";
+          ctx.textAlign = "center";
+          ctx.fillText(
+            formattedValue, // Use formatted value
+            margin.left + i * barWidth + barWidth / 2, // Center text horizontally
+            margin.top + (1 - d / Math.max(...this.data)) * height - 5 // Place text above the bar
+          );
+        }
+
+        // Draw the column labels
+        ctx.fillStyle = "black";
+        ctx.font = "12px Arial";
+        ctx.textAlign = "center";
+        ctx.fillText(
+          columnLabels[i], // Column label
+          margin.left + i * barWidth + barWidth / 2, // Center text horizontally
+          margin.top + height + 20 // Place text below the x-axis
         );
-
-        }
-
-        //QUA
-
-        if (i==2) {
-          d = d -  this.data[i-1]  ;
-          ctx.fillStyle = "black";
-      ctx.font = "14px Arial";
-      ctx.textAlign = "center";
-      ctx.fillText(
-        d.toString(), // Convert value to string
-        margin.left + i * barWidth + barWidth / 2, // Center text horizontally
-        margin.top + (1 - (d  +this.data[i-1]) / Math.max(...this.data)) * height - 5 // Place text above the bar
-      );
-        }
-        else{
-          ctx.fillStyle = "black";
-      ctx.font = "14px Arial";
-      ctx.textAlign = "center";
-      ctx.fillText(
-        d.toString(), // Convert value to string
-        margin.left + i * barWidth + barWidth / 2, // Center text horizontally
-        margin.top + (1 - d / Math.max(...this.data)) * height - 5 // Place text above the bar
-      );
-        //QUA
-    }
-
-
-      ctx.fillStyle = "black";
-      ctx.font = "12px Arial";
-      ctx.textAlign = "center";
-      ctx.fillText(
-      columnLabels[i], // Column label
-      margin.left + i * barWidth + barWidth / 2, // Center text horizontally
-      margin.top + height + 20 // Place text below the x-axis
-    );
       }
-      
+
+      // Draw x-axis
       ctx.beginPath();
       ctx.moveTo(margin.left, margin.top + height);
       ctx.lineTo(margin.left + width, margin.top + height);
       ctx.strokeStyle = "black";
       ctx.lineWidth = 2;
       ctx.stroke();
-
-      
-
     }
   }
 
