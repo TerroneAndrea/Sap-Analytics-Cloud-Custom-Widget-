62336bebba983a5404be1d4fbf4973eb07e620a3
Andrea Terrone <122184178+TerroneAndrea@users.noreply.github.com>
2024-05-15 14:40:30 +0200

    Update CustomWidgetSaving.js

--- CustomWidgetSaving.js
+++ CustomWidgetSaving.js
@@ -161,9 +161,9 @@
           ctx.fillStyle = "rgba(255, 255, 255, 1)";
           ctx.fillRect(
             margin.left + i * barWidth + 10, // Add some separation between bars
-            margin.top + (1 - this.data[i+1] / Math.max(...this.data)) * height,
+            margin.top + (1 - (this.data[i+1]+this.data[i]) / Math.max(...this.data)) * height,
             barWidth - 20, // Reduce the width to include separation
-            d / Math.max(...this.data) * height
+            this.data[i+1] / Math.max(...this.data) * height
           );
 
         }
