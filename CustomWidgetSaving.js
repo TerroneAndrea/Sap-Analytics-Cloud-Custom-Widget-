3eba353a6f58e73f6879fadd82aeea8b94140464
Andrea Terrone <122184178+TerroneAndrea@users.noreply.github.com>
2024-05-15 14:32:09 +0200

    Update CustomWidgetSaving.js

--- CustomWidgetSaving.js
+++ CustomWidgetSaving.js
@@ -163,7 +163,7 @@
             margin.left + i * barWidth + 10, // Add some separation between bars
             margin.top + (1 - this.data[i] / Math.max(...this.data)) * height,
             barWidth - 20, // Reduce the width to include separation
-            this.data[i-1] / Math.max(...this.data) * height
+            d / Math.max(...this.data) * height
           );
 
         }
