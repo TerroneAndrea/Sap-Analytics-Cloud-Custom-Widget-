067484d03df29e772e1f537b6a9be98ea1945b30
Andrea Terrone <122184178+TerroneAndrea@users.noreply.github.com>
2024-05-15 14:25:32 +0200

    Update CustomWidgetSaving.js

--- CustomWidgetSaving.js
+++ CustomWidgetSaving.js
@@ -149,7 +149,7 @@
           
         else if (i===3) {
           let d = this.data[4];
-          d = this.data[i] + this.data[i-1] ;
+          d = this.data[i] + this.data[i+1] ;
           ctx.fillStyle = barColor;
           ctx.fillRect(
             margin.left + i * barWidth + 10, // Add some separation between bars
@@ -161,7 +161,7 @@
           ctx.fillStyle = "rgba(255, 255, 255, 1)";
           ctx.fillRect(
             margin.left + i * barWidth + 10, // Add some separation between bars
-            margin.top + (1 - this.data[i-1] / Math.max(...this.data)) * height,
+            margin.top + (1 - this.data[i+1] / Math.max(...this.data)) * height,
             barWidth - 20, // Reduce the width to include separation
             this.data[i-1] / Math.max(...this.data) * height
           );
